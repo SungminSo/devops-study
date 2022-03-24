85eca0ce4fd2ffe8b09d2ba8806d8bce132baec3
SungminSo <soos196@hanyang.ac.kr>
2022-03-24 23:09:09 +0900

    220324 part2 chapter2 14

--- part2/chapter2/order_project/boss/urls.py
+++ part2/chapter2/order_project/boss/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from boss import views
 
 urlpatterns = [
+    path('orders/<int:shop>', views.order_list, name='order_list'),
     path('estimated-time/', views.estimated_time_input, name='estimated_time_input'),
     # path('menus/<int:shop>', views.menu, name='menu'),
     # path('orders/', views.order, name='order'),
--- part2/chapter2/order_project/boss/views.py
+++ part2/chapter2/order_project/boss/views.py
@@ -8,13 +8,18 @@ from order.models import Order
 
 
 @csrf_exempt
-def estimated_time_input(request):
+def order_list(request, shop):
     if request.method == 'GET':
-        order_list = Order.objects.all()
+        order_list = Order.objects.filter(shop=shop)
         return render(request, 'boss/order_list.html', {'order_list': order_list})
-    elif request.method == 'POST':
-        print(request.POST.get('order_id'))
+
+
+@csrf_exempt
+def estimated_time_input(request):
+    if request.method == 'POST':
         order_item = Order.objects.get(pk=int(request.POST.get('order_id')))
         order_item.estimated_time = int(request.POST.get('estimated_time'))
         order_item.save()
-        return render(request, 'boss/success.html')
+        return render(request, 'boss/success.html', {'shop': int(order_item.shop.id)})
+    else:
+        return HttpResponse(status=405)
