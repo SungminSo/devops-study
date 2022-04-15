bea0fad1b299cd44b6b79ebffdb90a833ca270bc
SungminSo <soos196@hanyang.ac.kr>
2022-04-15 17:59:46 +0900

    220415 part2 chapter3 02

--- part2/chapter3/auth_server/user/urls.py
+++ part2/chapter3/auth_server/user/urls.py
@@ -5,4 +5,5 @@ from user import views
 
 urlpatterns = [
     path('user/', views.user, name='user'),
+    path('login/', views.user, name='login'),
 ]
--- part2/chapter3/auth_server/user/views.py
+++ part2/chapter3/auth_server/user/views.py
@@ -20,3 +20,16 @@ def user(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'user/login.html')
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        try:
+            request.session['user_id'] = User.objects.get(name=name).id
+            return render(request, 'user/success.html')
+        except:  # noqa
+            return render(request, 'user/fail.html')
